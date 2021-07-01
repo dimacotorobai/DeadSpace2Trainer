713a16e864cedf759c20b40dd4be5a7010b12b76
dimacotorobai <52585921+dimacotorobai@users.noreply.github.com>
2021-07-01 13:49:48 -0400

    Removed PyQt

--- window.py
+++ window.py
@@ -1,11 +1,20 @@
 import tkinter as tk
 
-#Make window
-window = tk.Tk()
-window.geometry('350x500')
+class Window():
+    def __init__(self, master):
+        self.master = master
+        master.title('Dead Space™ 2 Hack')
+        master.geometry('300x500')
 
-#Label
-greeting = tk.Label(text='Hello, Tkinter')
-greeting.pack()
+        self.label = tk.Label(master=master, text='Label1')
+        self.label.pack()
 
-window.mainloop()
+        self.greet_button = tk.Button(master=master, text='Press Me')
+        self.greet_button.pack()
+
+        self.close_button = tk.Button(master=master, text='Close App', command = master.quit)
+        self.close_button.pack()
+
+root = tk.Tk()
+my_gui = Window(root)
+root.mainloop()
